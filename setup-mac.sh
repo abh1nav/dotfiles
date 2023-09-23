723610339ea8e37a787de1e2677cc1406e19f740
Abhinav Ajgaonkar <abhinav316@gmail.com>
2023-09-22 22:44:20 -0400

    Move unused configs into archive

--- setup-mac.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-brew update
-brew install exa 
-brew install git-cola
-brew install derailed/k9s/k9s
--- setup.sh
+++ setup.sh
@@ -19,43 +19,6 @@ cd ~
 ln -s ~/.dotfiles/.zshrc
 touch .localzshrc
 
-# Tmux setup
-echo Setting up tmux
-cd ~
-ln -s ~/.dotfiles/.tmux.conf
-
-# Vim setup
-echo Vim setup
-ln -s ~/.dotfiles/.vimrc
-mkdir ~/.vim
-cd ~/.vim
-ln -s ~/.dotfiles/.vim/autoload
-ln -s ~/.dotfiles/.vim/colors
-mkdir bundle
-echo Installing vim plugins
-cd bundle
-echo Installing NERDTree
-git clone https://github.com/scrooloose/nerdtree.git
-echo Installing CtrlP
-git clone https://github.com/kien/ctrlp.vim.git
-echo Installing Airline
-git clone https://github.com/bling/vim-airline.git
-echo Installing vim-go
-git clone https://github.com/fatih/vim-go.git
-echo Installing vim-fugitive \(For git branch display\)
-git clone https://github.com/tpope/vim-fugitive.git
-echo Installing Bbye \(Close buffers without deleting the window\)
-git clone https://github.com/moll/vim-bbye.git
-
-# Install tools based on OS
-if [[ $(uname) == "Darwin" ]]; then
-    source ./setup-mac.sh
-# elif command -v apt > /dev/null; then
-#     source ./setup-deb.sh
-else
-    echo 'skipping tools install'
-fi
-
 # Fonts
 echo Look in the fonts directory to install patched powerline fonts for powerline to work properly.
 echo Also, setup iTerms profile to point to the patched version of Droid Sans Mono.
