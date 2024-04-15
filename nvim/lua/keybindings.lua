a7bdf13222ef6fe9e58010f1d020d6aa6975a933
Abhinav Ajgaonkar <abhinav316@gmail.com>
2024-04-15 17:30:41 -0400

    Add undotree.

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -1,3 +1,3 @@
 -- nvim-tree
-vim.keymap.set("n", "<C-n>", "<cmd>:NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
+vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "Nvimtree Focus window" })
