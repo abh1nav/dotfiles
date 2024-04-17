027b729dd2ac905ceea68b97f2ad2615cf74a5f6
Abhinav Ajgaonkar <abhinav316@gmail.com>
2024-04-17 10:59:16 -0400

    Add buffer cycling using bufferline-cycle-windowless

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -3,6 +3,22 @@ vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggl
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "Nvimtree Focus window" })
 
 -- buffer management
-vim.keymap.set("n", "<leader>bx", "<cmd>bdelete<CR>", { desc = "[B]uffer [X]Close" })
-vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "[B]uffer [N]ext" })
-vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "[B]uffer [P]revious" })
+vim.keymap.set("n", "<leader>x", "<cmd>bdelete<CR>", { desc = "[B]uffer [X]Close", noremap = true })
+vim.keymap.set(
+  "n",
+  "<S-l>",
+  "<CMD>BufferLineCycleWindowlessNext<CR>",
+  { desc = "Buffer next", noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<S-h>",
+  "<CMD>BufferLineCycleWindowlessPrev<CR>",
+  { desc = "Buffer previous", noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<S-t>",
+  "<CMD>BufferLineCycleWindowlessToggle<CR>",
+  { desc = "Toggle bufferline-cycle-windowless", noremap = true, silent = true }
+)
