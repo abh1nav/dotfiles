14c726a94a168491eb00b863f01d391429a92ff1
Abhinav Ajgaonkar <abhinav316@gmail.com>
2024-12-10 10:18:56 -0500

    Apply language specific tabstop for JS/TS

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -6,3 +6,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- Language specific settings
+local function set_language_config()
+  local filetype = vim.bo.filetype
+
+  if filetype == "javscript" or filetype == "typescript" then
+    vim.opt.tabstop = 4
+    vim.opt.shiftwidth = 4
+    vim.opt.softtabstop = 4
+    vim.opt.expandtab = true
+  end
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "*" },
+  callback = function()
+    set_language_config()
+  end,
+})
